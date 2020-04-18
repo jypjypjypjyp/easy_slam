496e77aa24c8f1a972c2366cba1d9745593c21dd
Jia Yupeng <jypjypjypjyp@hotmail.com>
2020-04-18 22:25:23 +0800

    创建仓库

---
+++ include/easy_slam/viewer.h
@@ -0,0 +1,63 @@
+//
+// Created by gaoxiang on 19-5-4.
+//
+
+#ifndef easy_slam_VIEWER_H
+#define easy_slam_VIEWER_H
+
+#include <thread>
+#include <pangolin/pangolin.h>
+
+#include "easy_slam/common_include.h"
+#include "easy_slam/frame.h"
+#include "easy_slam/map.h"
+
+namespace easy_slam {
+
+/**
+ * 可视化
+ */
+class Viewer {
+   public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+    typedef std::shared_ptr<Viewer> Ptr;
+
+    Viewer();
+
+    void SetMap(Map::Ptr map) { map_ = map; }
+
+    void Close();
+
+    // 增加一个当前帧
+    void AddCurrentFrame(Frame::Ptr current_frame);
+
+    // 更新地图
+    void UpdateMap();
+
+   private:
+    void ThreadLoop();
+
+    void DrawFrame(Frame::Ptr frame, const float* color);
+
+    void DrawMapPoints();
+
+    void FollowCurrentFrame(pangolin::OpenGlRenderState& vis_camera);
+
+    /// plot the features in current frame into an image
+    cv::Mat PlotFrameImage();
+
+    Frame::Ptr current_frame_ = nullptr;
+    Map::Ptr map_ = nullptr;
+
+    std::thread viewer_thread_;
+    bool viewer_running_ = true;
+
+    std::unordered_map<unsigned long, Frame::Ptr> active_keyframes_;
+    std::unordered_map<unsigned long, MapPoint::Ptr> active_landmarks_;
+    bool map_updated_ = false;
+
+    std::mutex viewer_data_mutex_;
+};
+}  // namespace easy_slam
+
+#endif  // easy_slam_VIEWER_H
---
+++ include/easy_slam/visual_odometry.h
@@ -0,0 +1,57 @@
+#pragma once
+#ifndef easy_slam_VISUAL_ODOMETRY_H
+#define easy_slam_VISUAL_ODOMETRY_H
+
+#include "easy_slam/backend.h"
+#include "easy_slam/common_include.h"
+#include "easy_slam/dataset.h"
+#include "easy_slam/frontend.h"
+#include "easy_slam/viewer.h"
+
+namespace easy_slam {
+
+/**
+ * VO 对外接口
+ */
+class VisualOdometry {
+   public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+    typedef std::shared_ptr<VisualOdometry> Ptr;
+
+    /// constructor with config file
+    VisualOdometry(std::string &config_path);
+
+    /**
+     * do initialization things before run
+     * @return true if success
+     */
+    bool Init();
+
+    /**
+     * start vo in the dataset
+     */
+    void Run();
+
+    /**
+     * Make a step forward in dataset
+     */
+    bool Step();
+
+    /// 获取前端状态
+    FrontendStatus GetFrontendStatus() const { return frontend_->GetStatus(); }
+
+   private:
+    bool inited_ = false;
+    std::string config_file_path_;
+
+    Frontend::Ptr frontend_ = nullptr;
+    Backend::Ptr backend_ = nullptr;
+    Map::Ptr map_ = nullptr;
+    Viewer::Ptr viewer_ = nullptr;
+
+    // dataset
+    Dataset::Ptr dataset_ = nullptr;
+};
+}  // namespace easy_slam
+
+#endif  // easy_slam_VISUAL_ODOMETRY_H
